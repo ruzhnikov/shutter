e15472d08682c3e09ebbd6fca9cf39de65e0121a
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2021-12-04 00:35:21 +0300

    * new bunch of functions for Shutter::Draw::Utils

--- share/shutter/resources/modules/Shutter/Draw/Utils.pm
+++ share/shutter/resources/modules/Shutter/Draw/Utils.pm
@@ -23,11 +23,13 @@
 package Shutter::Draw::Utils;
 
 use 5.010;
+use utf8;
 use strict;
 use warnings;
 
 use Gtk3;
 use GooCanvas2;
+use Glib qw/ TRUE FALSE /;
 
 sub points_to_canvas_points {
     my @points = @_;
@@ -40,6 +42,67 @@ sub points_to_canvas_points {
     }
 
     return $result;
+}
+
+sub check_valid_mime_type {
+    my $mime_type = shift;
+
+    for my $format ( Gtk3::Gdk::Pixbuf::get_formats() ) {
+        for my $mime ( @{ $format->get_mime_types } ) {
+            return TRUE if $mime_type eq $mime_type;
+            last;
+        }
+    }
+
+    return FALSE;
+}
+
+sub utf8_decode {
+    my $string = shift;
+
+    #see https://bugs.launchpad.net/shutter/+bug/347821
+    utf8::decode $string;
+
+    return $string;
+}
+
+sub modify_text_in_properties {
+    my $font_btn       = shift;
+    my $textview       = shift;
+    my $font_color     = shift;
+    my $item           = shift;
+    my $use_font       = shift;
+    my $use_font_color = shift;
+
+    my $font_descr = Pango::FontDescription->from_string( $font_btn->get_font_name );
+    my $texttag    = Gtk3::TextTag->new;
+
+    if ( $use_font->get_active && $use_font_color->get_active ) {
+        $texttag->set( 'font-desc' => $font_descr, 'foreground-rgba' => $font_color->get_rgba );
+    } elsif ( $use_font->get_active ) {
+        $texttag->set( 'font-desc' => $font_descr );
+    } elsif ( $use_font_color->get_active ) {
+        $texttag->set( 'foreground-rgba' => $font_color->get_rgba );
+    }
+
+    my $texttagtable = Gtk3::TextTagTable->new;
+    $texttagtable->add($texttag);
+
+    my $text = Gtk3::TextBuffer->new($texttagtable);
+    $text->signal_connect(
+        'changed' => sub {
+            $text->apply_tag( $texttag, $text->get_start_iter, $text->get_end_iter );
+        } );
+
+    $text->set_text(
+        $textview->get_buffer->get_text(
+            $textview->get_buffer->get_start_iter,
+            $textview->get_buffer->get_end_iter, FALSE
+        ) );
+    $text->apply_tag( $texttag, $text->get_start_iter, $text->get_end_iter );
+    $textview->set_buffer($text);
+
+    return TRUE;
 }
 
 1;
